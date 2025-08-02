bbbdc27614dd9c84234eaf4fb7c2bf0dabfcc9bd
Abdul Rafay Ahmed <111084742+Rafay575@users.noreply.github.com>
2025-08-03 00:59:58 +0500

    add

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:3000/api"
+export const baseUrl = "http://www.outcasters.uk/api"
 
-export const API_URL = "http://localhost:3000"; // (for dev, or use import.meta.env for prod)
+export const API_URL = "http://www.outcasters.uk"; // (for dev, or use import.meta.env for prod)
